699562aa5529577a5ba74fcd0c03b77a0ea1fa23
Adriano D Machado <adriano@systax.com.br>
2026-04-26 01:07:02 -0300

    add laravel skills

---
+++ app/DTO/User/CreateUserDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTO\User;
+
+final readonly class CreateUserDTO
+{
+    public function __construct(
+        public string $name,
+        public string $email,
+        public string $password,
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            name: $data['name'],
+            email: $data['email'],
+            password: $data['password'],
+        );
+    }
+}
